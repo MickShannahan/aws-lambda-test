116243cea8f77c58ff7942812f4d604ad417d069
mick shannahan <beasticorn@gmail.com>
2024-01-29 22:36:20 -0700

    aws lambda test

---
+++ aws-test/hello-world/app.mjs
@@ -0,0 +1,26 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} request - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+export const lambdaHandler = async (request, context) => {
+    try {
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify({
+              request
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
